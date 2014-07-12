fdcfbe60d0eda0da71df35c78dcce8d8be701f09
Ibai <ibai.gilabert@gmail.com>
2014-07-13 00:39:27 +0200

    C++

--- env_path.pl
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/perl 
-$path =  $ENV{'PATH'}; 
-print "path: $path\n"; 
--- throw.cc
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-using namespace std;
-
-int main() {
- fprintf(stderr, "I am in stderr\n");
- }
