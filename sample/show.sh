63f8d750f7eb8594c5036aacd8ffcbdd005a7b3a
Ibai Gilabert <ibai.gilabert@gmail.com>
2015-01-28 00:30:50 +0100

    reporting

---
+++ sample/show.sh
@@ -0,0 +1,23 @@
+echo ROUGE; cat Asiya-1.config.metrics_ROUGE.err Asiya-2.config.metrics_ROUGE.err Asiya-5.config.metrics_ROUGE.err Asiya-10.config.metrics_ROUGE.err Asiya-all.config.metrics_ROUGE.err | grep TOTAL
+echo -e "-------------------------\n";
+
+echo METEOR; cat Asiya-1.config.metrics_METEOR.err Asiya-2.config.metrics_METEOR.err Asiya-5.config.metrics_METEOR.err Asiya-10.config.metrics_METEOR.err Asiya-all.config.metrics_METEOR.err | grep TOTAL
+echo -e "-------------------------\n";
+
+echo GTM; cat Asiya-1.config.metrics_GTM.err Asiya-2.config.metrics_GTM.err Asiya-5.config.metrics_GTM.err Asiya-10.config.metrics_GTM.err Asiya-all.config.metrics_GTM.err | grep TOTAL
+echo -e "-------------------------\n";
+
+echo O; cat Asiya-1.config.metrics_O.err Asiya-2.config.metrics_O.err Asiya-5.config.metrics_O.err Asiya-10.config.metrics_O.err Asiya-all.config.metrics_O.err | grep TOTAL
+echo -e "-------------------------\n";
+
+echo WER; cat Asiya-1.config.metrics_WER.err Asiya-2.config.metrics_WER.err Asiya-5.config.metrics_WER.err Asiya-10.config.metrics_WER.err Asiya-all.config.metrics_WER.err | grep TOTAL
+echo -e "-------------------------\n";
+
+echo PER; cat Asiya-1.config.metrics_PER.err Asiya-2.config.metrics_PER.err Asiya-5.config.metrics_PER.err Asiya-10.config.metrics_PER.err Asiya-all.config.metrics_PER.err | grep TOTAL
+echo -e "-------------------------\n";
+
+echo TER; cat Asiya-1.config.metrics_TER.err Asiya-2.config.metrics_TER.err Asiya-5.config.metrics_TER.err Asiya-10.config.metrics_TER.err Asiya-all.config.metrics_TER.err | grep TOTAL
+echo -e "-------------------------\n";
+
+echo LEM; cat Asiya-1.config.metrics_LEM.err Asiya-2.config.metrics_LEM.err Asiya-5.config.metrics_LEM.err Asiya-10.config.metrics_LEM.err Asiya-all.config.metrics_LEM.err | grep TOTAL
+echo -e "-------------------------\n";
